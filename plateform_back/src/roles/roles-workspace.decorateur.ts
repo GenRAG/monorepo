4bb09e3e630a786280bcf930c89852e46567e970
quentinbol <quentin.bollore@epitech.eu>
2025-06-13 21:59:52 +0200

    feat: add WorkspaceRolesGuard and RolesInWorkspace decorator for role-based access control

---
+++ plateform_back/src/roles/roles-workspace.decorateur.ts
@@ -0,0 +1,6 @@
+import { SetMetadata } from '@nestjs/common';
+import { UserRole } from 'generated/prisma';
+
+export const ROLES_IN_WORKSPACE_KEY = 'roles_in_workspace';
+export const RolesInWorkspace = (...roles: UserRole[]) =>
+    SetMetadata(ROLES_IN_WORKSPACE_KEY, roles);

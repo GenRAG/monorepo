6cc599daf8e7c22a2576afa1ee866f18de354cde
quentinbol <quentin.bollore@epitech.eu>
2025-10-25 16:28:07 -0400

    feat: restructure App and Router components for improved layout and routing

--- plateform_front/src/app/App.tsx
+++ plateform_front/src/app/App.tsx
@@ -1,9 +1,12 @@
+import { Box, useColorModeValue } from '@chakra-ui/react';
 import Router from 'app/Router';
 import React from 'react';
 
 const App: React.FC = () => {
   return (
-    <Router />
+    <Box bg={useColorModeValue("gray.100", "gray.900")} minH="100vh">
+      <Router />
+    </Box>
   );
 };
 
--- plateform_front/src/index.tsx
+++ plateform_front/src/index.tsx
@@ -3,21 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import themeNew from 'themeNew/index';
 import { Provider } from 'react-redux';
 import { store } from './store'
+import ReduxProvider from 'store/reduxProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ChakraProvider theme={themeNew} resetCSS>
-        <App />
-      </ChakraProvider>
-    </Provider>
+      <ReduxProvider>
+        <ChakraProvider theme={themeNew} resetCSS>
+          <ColorModeScript initialColorMode={themeNew.config.initialColorMode} />
+          <App />
+        </ChakraProvider>
+      </ReduxProvider>
   </React.StrictMode>
 );
 

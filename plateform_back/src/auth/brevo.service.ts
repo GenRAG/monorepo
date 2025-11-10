019e7c55869790e66ed219d384c5da56b1a4b4b1
quentinbol <quentin.bollore@epitech.eu>
2025-11-10 13:50:20 -0400

    feat: add sendPasswordResetEmail method to BrevoService for password reset functionality

--- plateform_back/src/auth/brevo.service.ts
+++ plateform_back/src/auth/brevo.service.ts
@@ -30,4 +30,18 @@ export class BrevoService {
 
         await this.apiInstance.sendTransacEmail(email);
     }
+
+    async sendPasswordResetEmail(
+        to: string,
+        token: number | null,
+    ): Promise<void> {
+        const email: Brevo.SendSmtpEmail = {
+            to: [{ email: to }],
+            sender: { name: 'GenRAG', email: 'quentinbollore@gmail.com' },
+            subject: 'Réinitialisation de votre mot de passe',
+            htmlContent: `<p>Bonjour,</p><p>Vous avez demandé une réinitialisation de votre mot de passe. Veuillez cliquez sur ce lien pour réinitialiser votre mot de passe : <strong>http://localhost:3000/new-password?token=${token}&email=${to}</strong></p><p>À bientôt,<br/>L'équipe GenRAG</p>`,
+        };
+
+        await this.apiInstance.sendTransacEmail(email);
+    }
 }

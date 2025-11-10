190f4fa5af43e18fca01cdcb8eb00e079a05cd68
quentinbol <quentin.bollore@epitech.eu>
2025-11-10 13:54:44 -0400

    feat: refactor PasswordForm to simplify code and improve error handling

--- plateform_front/src/pages/Auth/Login/PasswordForm.tsx
+++ plateform_front/src/pages/Auth/Login/PasswordForm.tsx
@@ -1,56 +1,35 @@
 import { FC } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link as ReachLink, useLocation, useSearchParams } from 'react-router-dom';
+import { Link as ReachLink, useLocation, useNavigate } from 'react-router-dom';
 import {
 	Button,
 	chakra,
-	Checkbox,
 	FormControl,
 	FormErrorMessage,
 	FormLabel,
 	Input,
 	Link,
 	Text,
+	useColorModeValue,
 	VStack,
 } from '@chakra-ui/react';
 import { AuthHeader } from 'pages/Auth/AuthHeader';
 import { LoginFormSteps } from 'pages/Auth/Layout/AuthLayout';
-//import { LocalStorageKeys } from 'types/localStorage';
-//import { useLocalStorage } from 'usehooks-ts';
-
 import { ShowHidePasswordInput } from 'components/Molecules/Inputs/ShowHidePasswordInput';
-//import { isParentDemoEnv } from 'demo/utils';
-//import { useFcmTokenManager } from 'hooks/auth/useFcmTokenManager';
-//import { useTokenManager } from 'hooks/auth/useTokenManager';
-//import { getAppEnvironment } from 'hooks/getAppEnvironment';
-//import useThemedToast from 'hooks/useThemedToast';
-//import { isNativeApp } from 'mobile/utils';
-//import { getConnectionDuration, useLazyLoginQuery } from 'services/requests/auth';
-//import { useHasPasskeysQuery } from 'services/requests/auth/passkeys';
 import colors from 'themeNew/foundations/colors';
 import { useLoginMutation } from 'services/auth/auth';
 import useThemedToast from 'hooks/useThemedToast';
 
 type PasswordFormType = {
 	password: string;
-	stayConnected: boolean; // rester connecté
+	stayConnected: boolean;
 };
-
-//const { isLocal } = getAppEnvironment();
 
 export const PasswordForm: FC<{ email: string; currentStep: LoginFormSteps }> = ({ email, currentStep }) => {
 	const location = useLocation();
 	const toast = useThemedToast();
-	const [searchParams] = useSearchParams();
 	const [login, { isLoading: isLoggingIn }] = useLoginMutation();
-
-	//const [doNotAskOnThisDevice] = useLocalStorage(LocalStorageKeys.AUTH.PASSKEYS.DO_NOT_ASK_ON_THIS_DEVICE, false);
-	//const { handleFcmToken } = useFcmTokenManager();
-
-	//const [login, { isLoading: isLoggingIn }] = useLazyLoginQuery();
-	//const { data: emailHasKeys } = useHasPasskeysQuery({ email });
-
-	//const { redeemToken } = useTokenManager();
+	const navigate = useNavigate();
 
 	const {
 		formState: { errors },
@@ -59,7 +38,6 @@ export const PasswordForm: FC<{ email: string; currentStep: LoginFormSteps }> = 
 	} = useForm<PasswordFormType>({
 		defaultValues: {
 			stayConnected: false,
-			//password: isLocal ? '12345678' : '',
 		},
 	});
 
@@ -67,30 +45,25 @@ export const PasswordForm: FC<{ email: string; currentStep: LoginFormSteps }> = 
 		login({
 			email,
 			password: data.password,
-		})
-			.unwrap()
-			.then(() => {
+		}).unwrap().then(() => {
 			toast({
-				title: 'Connexion réussie',
+				title: 'Successfully logged in!',
+				description: 'Welcome back',
 				status: 'success',
 				duration: 3000,
 				isClosable: true,
 			});
-			})
-			.catch((err) => {
-				let message = 'Impossible de se connecter.';
-				console.log(err)
-
-				// RTK Query FetchBaseQueryError
-
-				toast({
-					title: "Une erreur s'est produite.",
-					description: err.data.error.message || message,
-					status: 'error',
-					duration: 9000,
-					isClosable: true,
-				});
-				});
+			navigate('/');
+		}).catch((err) => {
+			let message = 'Please try again later.';
+			toast({
+				title: "An error occurred.",
+				description: err.data.error.message || message,
+				status: 'error',
+				duration: 9000,
+				isClosable: true,
+			});
+		});
 
 	});
 
@@ -113,17 +86,12 @@ export const PasswordForm: FC<{ email: string; currentStep: LoginFormSteps }> = 
 									minLength: 8,
 									maxLength: 100,
 								})}
-								placeholder="Minimum 8 caractères"
+								placeholder="Minimum 8 characters"
 								autoComplete="new-password"
+								color={useColorModeValue("black", "whites.offwhite")}
 							/>
 							{!!errors.password && <FormErrorMessage>{errors.password?.message}</FormErrorMessage>}
 						</FormControl>
-
-						{/*!isNativeApp && (
-							<Checkbox {...register('stayConnected')} variant="simple" w="100%">
-								<Text variant="body-sm">Rester connecté</Text>
-							</Checkbox>
-						)*/}
 					</VStack>
 					<Button
 						color="whites.offwhite"
@@ -137,7 +105,7 @@ export const PasswordForm: FC<{ email: string; currentStep: LoginFormSteps }> = 
 						Sign in
 					</Button>
 					<VStack w="100%" justifyContent="center">
-						<Link as={ReachLink} to={'/mot-de-passe-oublie' + location.search}>
+						<Link as={ReachLink} to={'/reset-password' + location.search}>
 							<Text variant="body-sm-semibold" color="whites.offwhite">Forgot password?</Text>
 						</Link>
 					</VStack>

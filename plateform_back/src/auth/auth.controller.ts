71ac58cd46e6abe1d2f4ea8afc632cde62a54236
quentinbol <quentin.bollore@epitech.eu>
2025-11-10 13:49:50 -0400

    feat: enhance AuthController with password reset and verification token functionalities

--- plateform_back/src/auth/auth.controller.ts
+++ plateform_back/src/auth/auth.controller.ts
@@ -5,11 +5,19 @@ import { User } from 'generated/prisma';
 import { Response } from 'express';
 import { AuthService } from 'src/auth/auth.service';
 import { CreateUserRequest } from 'src/users/dto/create-user.request';
-import { VerifyTokenRequest } from 'src/users/dto/verify-token.request';
+import {
+    NewPasswordRequest,
+    ResendVerifyTokenRequest,
+    VerifyTokenRequest,
+} from 'src/users/dto/verify-token.request';
+import { TokenService } from 'src/auth/token.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(
+        private readonly authService: AuthService,
+        private readonly tokenService: TokenService,
+    ) {}
 
     @UseGuards(LocalAuthGuard)
     @Post('login')
@@ -26,10 +34,32 @@ export class AuthController {
     }
 
     @Post('verification-token')
-    checkVerificationToken(
+    async checkVerificationToken(
         @Body() verifyTokenBody: VerifyTokenRequest,
         @Res({ passthrough: true }) response: Response,
     ) {
-        return this.authService.verifyEmailToken(verifyTokenBody, response);
+        const user = await this.tokenService.verifyEmailToken(verifyTokenBody);
+        return this.login(user, response);
+    }
+
+    @Post('resend-verification-token')
+    resendVerificationToken(@Body() verifyTokenBody: ResendVerifyTokenRequest) {
+        return this.tokenService.generateAndSendVerificationToken(
+            verifyTokenBody.email,
+        );
+    }
+
+    @Post('reset-password')
+    resetPassword(@Body() resetPasswordBody: ResendVerifyTokenRequest) {
+        console.log(
+            'Received reset password request for email:',
+            resetPasswordBody.email,
+        );
+        return this.authService.initiatePasswordReset(resetPasswordBody.email);
+    }
+
+    @Post('verify-password-reset-token')
+    verifyPasswordResetToken(@Body() verifyTokenBody: NewPasswordRequest) {
+        return this.authService.verifyPasswordResetToken(verifyTokenBody);
     }
 }

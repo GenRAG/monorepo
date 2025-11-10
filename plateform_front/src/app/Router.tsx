d0b6531fd09df254d777a3f337d4d24931c28d3d
quentinbol <quentin.bollore@epitech.eu>
2025-11-10 13:53:15 -0400

    feat: add routes for reset password and new password with private layout

--- plateform_front/src/app/Router.tsx
+++ plateform_front/src/app/Router.tsx
@@ -3,6 +3,11 @@ import Login from "pages/Auth/Login";
 import Register from "pages/Auth/Register";
 import AuthLayout from "pages/Auth/Layout/AuthLayout";
 import Validate from "pages/Auth/Validate";
+import ResetPassword from "pages/Auth/Password";
+import ApplyResetPassword from "pages/Auth/Password/ApplyResetPassword";
+import PrivateRoute from "app/PrivateRoute";
+import Sidebar from "app/Navigation/Sidebar";
+import PrivateAppLayout from "app/PrivateAppLayout";
 
 export default function Router() {
   return (
@@ -12,7 +17,17 @@ export default function Router() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/validate" element={<Validate />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/new-password" element={<ApplyResetPassword />} />
         </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route element={<PrivateAppLayout />}>
+            <Route path="/" element={<div>YO</div>} />
+            <Route path="/dashboard" element={<div>DASHBOARD</div>} />
+          </Route>
+        </Route>
+
 
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>

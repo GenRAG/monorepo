510c997f6b2dfbf0bff61f68a68a4f294444394e
quentinbol <quentin.bollore@epitech.eu>
2026-01-13 15:38:07 -0400

    feat: integrate OnboardingProvider and stepsConfig into PrivateAppLayout

--- plateform_front/src/app/PrivateAppLayout.tsx
+++ plateform_front/src/app/PrivateAppLayout.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "app/Navigation/Sidebar";
 import { Box, Flex } from "@chakra-ui/react";
+import { OnboardingProvider } from "pages/Onboarding/OnBoardingProvider";
+import { stepsConfig } from "pages/Onboarding/steps/StepConfig";
 
 const PrivateAppLayout: React.FC = () => {
   return (
-    <Flex minH="100vh">
-      <Sidebar />
-      <Box flex={1} p={4}>
-        <Outlet />
+    <Flex minH="100vh" w="100%">
+      {/*<Sidebar />*/}
+      <Box flex={1}>
+        <OnboardingProvider steps={stepsConfig}>
+          <Outlet />
+        </OnboardingProvider>
       </Box>
     </Flex>
   );

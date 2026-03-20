212433a802d08151a5317cb3b302d726e8403079
quentinbol <quentin.bollore@epitech.eu>
2026-03-20 02:53:51 -0400

    feat: create DeleteUserRequest DTO for user deletion and enhance CreateUserRequest with API property decorators

--- plateform_back/src/users/dto/create-user.request.ts
+++ plateform_back/src/users/dto/create-user.request.ts
@@ -1,13 +1,26 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsStrongPassword } from 'class-validator';
 import { Prisma } from 'generated/prisma';
 
 export class CreateUserRequest {
+    @ApiProperty({
+        example: 'user@example.com',
+        description: 'User email address',
+    })
     @IsEmail()
     email: string;
 
+    @ApiProperty({
+        example: 'StrongP@ssw0rd!',
+        description: 'Strong password',
+    })
     @IsStrongPassword()
     password: string;
 
+    @ApiPropertyOptional({
+        example: 'Alice',
+        description: 'User display name',
+    })
     @IsOptional()
     name?: string;
 }

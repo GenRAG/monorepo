0cb3e58956481db6347255f9d1d119b8dc7a05f0
quentinbol <quentin.bollore@epitech.eu>
2026-03-20 02:52:54 -0400

    feat: integrate Swagger documentation and API reference in main application bootstrap

--- plateform_back/src/main.ts
+++ plateform_back/src/main.ts
@@ -5,21 +5,38 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
 import cookieParser from 'cookie-parser';
 import { AllExceptionsFilter } from 'src/exeptions/interceptor.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('GenRAG API')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+
     app.enableCors({
         origin: app.get(ConfigService).getOrThrow('FRONTEND_URL'),
         credentials: true,
     });
+
     app.useLogger(app.get(Logger));
+
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
         }),
     );
+
     app.useGlobalFilters(new AllExceptionsFilter(app.get(Logger)));
+
     app.use(cookieParser());
+
+    app.use('/docs', apiReference({ spec: { content: document } }));
+
     await app.listen(app.get(ConfigService).getOrThrow('PORT'));
 }
+
 void bootstrap();

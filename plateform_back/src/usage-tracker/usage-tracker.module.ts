bce72880428eff42cf524213f8a39f5c1ac697e9
quentinbol <quentin.bollore@epitech.eu>
2026-03-23 04:04:02 -0400

    feat: add PrismaModule to UsageTrackerModule imports

--- plateform_back/src/usage-tracker/usage-tracker.module.ts
+++ plateform_back/src/usage-tracker/usage-tracker.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CreditBalanceModule } from 'src/credit/credit-balance.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { CreditTransactionModule } from 'src/transaction/credit-transaction.module';
 import { UsageTrackerService } from 'src/usage-tracker/usage-tracker.service';
 
 @Module({
     controllers: [],
     providers: [UsageTrackerService],
-    imports: [CreditBalanceModule, CreditTransactionModule],
+    imports: [CreditBalanceModule, CreditTransactionModule, PrismaModule],
     exports: [UsageTrackerService],
 })
 export class UsageTrackerModule {}
